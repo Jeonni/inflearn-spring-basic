a080dc0707824cec5a3c67ebe9acfdebf369f170
Jeonni <jeonniu@naver.com>
2023-06-02 01:46:39 +0900

    Feat: AppConfig 역할에 따른 구현 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,12 +12,22 @@ import hello.core.order.OrderServiceImpl;
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    // 중복을 제거하고, 역할에 따른 구현이 보이도록 리팩토링(1)
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 중복을 제거하고, 역할에 따른 구현이 보이도록 리팩토링(2)
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
